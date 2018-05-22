900d5be1616e9ab5bb23d135078525ee737892d9
André Chaves <andrechavesg@gmail.com>
2018-05-22 14:10:55 -0300

    capitulo5

--- estoquews/src/br/com/caelum/estoque/ws/EstoqueWS.java
+++ estoquews/src/br/com/caelum/estoque/ws/EstoqueWS.java
@@ -6,6 +6,10 @@ import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.ParameterStyle;
+import javax.jws.soap.SOAPBinding.Style;
+import javax.jws.soap.SOAPBinding.Use;
 
 import br.com.caelum.estoque.modelo.item.Filtro;
 import br.com.caelum.estoque.modelo.item.Filtros;
@@ -18,7 +22,8 @@ import br.com.caelum.estoque.modelo.usuario.TokenDao;
 import br.com.caelum.estoque.modelo.usuario.TokenUsuario;
 
 @WebService
-public class EstoqueWS {
+@SOAPBinding(style=Style.DOCUMENT,use=Use.LITERAL,parameterStyle=ParameterStyle.WRAPPED)
+public class EstoqueWS { //RPC
 
 	private ItemDao dao = new ItemDao();
 
@@ -31,7 +36,7 @@ public class EstoqueWS {
 		return new ListaItens(itensResultado);
 	}
 
-	@WebMethod(operationName = "CadastrarItem")
+	@WebMethod(action="CadastrarItem",operationName = "CadastrarItem")
 	@WebResult(name = "item")
 	public Item cadastrarItem(@WebParam(name = "tokenUsuario", header = true) TokenUsuario token,
 			@WebParam(name = "item") Item item) throws AutorizacaoException {
